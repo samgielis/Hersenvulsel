5c7695746946d11df2fd53b93cd62ccee273f3a0
Sam Gielis <sam.gielis@student.kuleuven.be>
2016-02-18 17:11:13 +0100

    Directory Generator

---
+++ maintenance/directory-generator/src/HVDirectoryReader.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.simple.parser.ParseException;
+
+
+public class HVDirectoryReader {
+	
+	public static ArrayList<HVArticle> read(String path){
+		
+		File file = new File(path);
+		
+		String[] articles = file.list(new FilenameFilter() {
+		  @Override
+		  public boolean accept(File current, String name) {
+		    return new File(current, name).isDirectory();
+		  }
+		});
+		
+		ArrayList<HVArticle> directory = new ArrayList<HVArticle>();
+		
+		for (String article : articles)
+			try {
+				directory.add(HVArticle.fromJSON(path + "/" + article + "/descriptor.json"));
+			} catch (IOException | ParseException e) {
+				e.printStackTrace();
+			} 
+		
+		return directory;
+	}
+}
