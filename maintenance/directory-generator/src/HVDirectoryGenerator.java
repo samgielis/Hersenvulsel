f5ae661132cfa5ad3a3dd57792397d627999502c
Sam Gielis <sam.gielis@student.kuleuven.be>
2016-02-23 00:15:40 +0100

    updated directory generator

--- maintenance/directory-generator/src/HVDirectoryGenerator.java
+++ maintenance/directory-generator/src/HVDirectoryGenerator.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 public class HVDirectoryGenerator {
 
 	private static String[] categories = new String[]{	
@@ -9,12 +11,21 @@ public class HVDirectoryGenerator {
 		"faitsdivers", "entertainment"
 	};
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args){
 		for (String cat : categories) {
 			File d = new File("../../"+cat);
-			String path = d.getCanonicalPath();
-			ArrayList<HVArticle> articles = HVDirectoryReader.read(path);
-			HVDirectoryWriter.write(path, articles);
+			String path;
+			try {
+				path = d.getCanonicalPath();
+				ArrayList<HVArticle> articles = HVDirectoryReader.read(path);
+				HVDirectoryWriter.write(path, articles);
+				
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, "Het lezen van de categorie " + cat + " is mislukt", "Error",
+		                JOptionPane.ERROR_MESSAGE);
+			}			
 		}
+		JOptionPane.showMessageDialog(null, "Gelukt!", "Succes",
+                JOptionPane.INFORMATION_MESSAGE);
 	}
 }
--- maintenance/directory-generator/src/HVDirectoryReader.java
+++ maintenance/directory-generator/src/HVDirectoryReader.java
@@ -1,7 +1,10 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 
 import org.json.simple.parser.ParseException;
 
@@ -25,8 +28,15 @@ public class HVDirectoryReader {
 			if( !article.contains("#TEMPLATE"))
 				try {
 					directory.add(HVArticle.fromJSON(path + "/" + article + "/descriptor.json"));
-				} catch (IOException | ParseException e) {
-					e.printStackTrace();
+				} catch (ParseException e) {
+					JOptionPane.showMessageDialog(null, "Er is een probleem met de codering van " + article + "/descriptor.json", "Error",
+			                JOptionPane.ERROR_MESSAGE);
+				} catch (FileNotFoundException e) {
+					JOptionPane.showMessageDialog(null, "Het bestand " + path + "/" + article +" bestaat niet.", "Error",
+			                JOptionPane.ERROR_MESSAGE);
+				} catch (IOException e) {
+					JOptionPane.showMessageDialog(null, "Er is een probleem met het inlezen van " + article, "Error",
+			                JOptionPane.ERROR_MESSAGE);
 				} 
 		return directory;
 	}
