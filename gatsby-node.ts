fd454bf7d33fcb82138f5340ffd89bef59d12ee1
Sam Gielis <samgielis@gmail.com>
2020-08-14 12:35:21 +0200

    Add dummy implementation gatsby-node

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -4,4 +4,11 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+import { GatsbyNode } from 'gatsby';
+
 // You can delete this file if you're not using it
+const onCreateNode: GatsbyNode['onCreateNode'] = () => {
+    (() => {})();
+};
+
+export default onCreateNode;
