18f7ac6100629efd6cc3a783a9ed1611277df6e2
Sam Gielis <samgielis@gmail.com>
2020-08-14 11:59:55 +0200

    Further updates in migration script.

--- migration/migrate-articles.ts
+++ migration/migrate-articles.ts
@@ -20,34 +20,23 @@ const categories = [
     'entertainment',
 ];
 
-function migrateCategory(categoryName: string): void {
-    const legacyPath = `./docs/${categoryName}/`;
-    let articleCount = 0;
-
-    console.log(`Migrating ${categoryName}...`);
-
-    const entries: Dirent[] = readdirSync(legacyPath, {
-        withFileTypes: true,
-    });
-
-    entries.forEach((entry) => {
-        if (!entry.isDirectory()) {
-            return;
-        }
-
-        const articleId = entry.name;
-        const destinationPath = join(
-            __dirname,
-            `../data/${categoryName}/${articleId}/`
-        );
-
-        migrateDescriptor(articleId, legacyPath, destinationPath);
-        migrateImages(articleId, legacyPath, destinationPath);
-
-        articleCount++;
-    });
-
-    console.log(`Migrated ${articleCount} articles in ${categoryName}`);
+function writeFileSyncRecursive(
+    filename: string,
+    content: string | NodeJS.ArrayBufferView,
+    charset: string
+) {
+    const folders = filename.split(sep).slice(0, -1);
+    if (folders.length) {
+        // create folder path if it doesn't exist
+        folders.reduce((last, folder) => {
+            const folderPath = last ? last + sep + folder : folder;
+            if (!existsSync(folderPath)) {
+                mkdirSync(folderPath);
+            }
+            return folderPath;
+        });
+    }
+    writeFileSync(filename, content, charset);
 }
 
 function migrateDescriptor(
@@ -85,23 +74,34 @@ function migrateImages(
     });
 }
 
-function writeFileSyncRecursive(
-    filename: string,
-    content: string | NodeJS.ArrayBufferView,
-    charset: string
-) {
-    const folders = filename.split(sep).slice(0, -1);
-    if (folders.length) {
-        // create folder path if it doesn't exist
-        folders.reduce((last, folder) => {
-            const folderPath = last ? last + sep + folder : folder;
-            if (!existsSync(folderPath)) {
-                mkdirSync(folderPath);
-            }
-            return folderPath;
-        });
-    }
-    writeFileSync(filename, content, charset);
+function migrateCategory(categoryName: string): void {
+    const legacyPath = `./docs/${categoryName}/`;
+    let articleCount = 0;
+
+    console.log(`Migrating ${categoryName}...`);
+
+    const entries: Dirent[] = readdirSync(legacyPath, {
+        withFileTypes: true,
+    });
+
+    entries.forEach((entry) => {
+        if (!entry.isDirectory()) {
+            return;
+        }
+
+        const articleId = entry.name;
+        const destinationPath = join(
+            __dirname,
+            `../data/${categoryName}/${articleId}/`
+        );
+
+        migrateDescriptor(articleId, legacyPath, destinationPath);
+        migrateImages(articleId, legacyPath, destinationPath);
+
+        articleCount += 1;
+    });
+
+    console.log(`Migrated ${articleCount} articles in ${categoryName}`);
 }
 
 categories.forEach(migrateCategory);
