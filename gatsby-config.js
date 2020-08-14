d7547d943b5e61877e26c25f02b7bc51a7f9236b
Sam Gielis <samgielis@gmail.com>
2020-08-14 12:22:32 +0200

    Use TypeScript in Gatsby files.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,13 @@
-module.exports = {
-    siteMetadata: {
-        title: `Gatsby TypeScript + ESlint + Prettier Starter`,
-        description: `This Gatsby starter is an adaptation of the default Gatsby starter with TypeScript, ESlint and Prettier pre-configured, bringing you everything you need to get up and running with Gatsby in a type-safe and style-safe way.`,
-        author: `@Tielem <ttielu@gmail.com>`,
+
+// We register the TypeScript evaluator in gatsby-config so we don't need to do
+// it in any other .js file. It automatically reads TypeScript config from
+// tsconfig.json.
+require('ts-node').register({
+    compilerOptions: {
+        module: 'commonjs',
+        target: 'es2017',
     },
-    plugins: [
-        {
-            resolve: `gatsby-plugin-ts`,
-            options: {
-                tsLoader: {
-                    logLevel: `warn`,
-                },
-                forkTsCheckerPlugin: {
-                    eslint: true,
-                },
-                codegen: false,
-            },
-        },
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-            },
-        },
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
-    ],
-};
+});
+
+// Use a TypeScript version of gatsby-config.js.
+module.exports = require('./gatsby-config.ts');
