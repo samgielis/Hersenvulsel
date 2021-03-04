ed843691000ffe64e7367fa589479837c5627112
Sam Gielis <sam.gielis@trustedfamily.net>
2021-03-04 23:33:35 +0100

    Render basic article content
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/templates/ArticlePage.tsx
+++ src/templates/ArticlePage.tsx
@@ -4,12 +4,21 @@ import React from 'react';
 export default function ArticlePage({
     data,
 }: PageProps<any, any>): JSX.Element {
-    return <div>This is an article, {data?.markdownRemark?.fields?.slug}</div>;
+    return (
+        <>
+            <div>This is an article, {data?.markdownRemark?.fields?.slug}</div>
+            <div
+                className="blog-post-content"
+                dangerouslySetInnerHTML={{ __html: data?.markdownRemark?.html }}
+            />
+        </>
+    );
 }
 
 export const query = graphql`
     query($slug: String!) {
         markdownRemark(fields: { slug: { eq: $slug } }) {
+            html
             fields {
                 slug
             }
