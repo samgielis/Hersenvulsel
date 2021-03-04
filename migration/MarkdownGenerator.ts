0ee2495855c14810bdb36a79f23fa1028a04dbe5
Sam Gielis <sam.gielis@trustedfamily.net>
2021-03-04 23:18:09 +0100

    Fix migration of bold and italic text
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- migration/MarkdownGenerator.ts
+++ migration/MarkdownGenerator.ts
@@ -55,8 +55,8 @@ function replaceHTMLAnchorsWithMDLinks(paragraph: string): string {
 
 function convertParagraphItemToMD(paragraph: ParagraphItem): string {
     const rawContent = paragraph.content;
-    let content = rawContent.replace('<b>', '**').replace('</b>', '**');
-    content = content.replace('<i>', '_').replace('</i>', '_');
+    let content = rawContent.replace(/<b>/g, '**').replace(/<\/b>/g, '**');
+    content = content.replace(/<i>/g, '_').replace(/<\/i>/, '_');
     return replaceHTMLAnchorsWithMDLinks(content);
 }
 
