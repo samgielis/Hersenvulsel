8e94b5c8a1ec63759115efd9fc2b1f9107987f61
Sam Gielis <sam.gielis@trustedfamily.net>
2021-03-08 23:21:59 +0100

    Remove unused viewport
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -80,15 +80,13 @@ const SEO = ({
                 },
             ].concat(meta)}
         >
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1"
-            />
             <link
+                type="text/css"
                 rel="stylesheet"
                 href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
             />
             <link
+                type="text/css"
                 rel="stylesheet"
                 href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"
             />
