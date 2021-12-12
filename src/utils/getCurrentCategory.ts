dcb615bb2c1107711cbe8db4c4366ffc9eaf8ac0
Sam Gielis <sam.gielis@trustedfamily.net>
2021-12-12 20:36:38 +0100

    Check if window available
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/utils/getCurrentCategory.ts
+++ src/utils/getCurrentCategory.ts
@@ -2,9 +2,9 @@ import { Category } from "../types/Category";
 
 const allCategories: Category[] = ["natuur", "entertainment", "faitsdivers", "wetenschap", "mensen", "geschiedenis"];
 export function getCurrentCategory(): Category | "default" {
-    const currentLocation = window.location.href;
+    const currentLocation = typeof window !== 'undefined' ? window.location.href : "";
     for (let category of allCategories) {
-        if (currentLocation.indexOf(`/${category}/`) > -1) {
+        if (currentLocation?.indexOf(`/${category}/`) > -1) {
             return category;
         }
     }
