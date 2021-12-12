bb92aeba4a999bc6944c15cf8025ee9c9365287e
Sam Gielis <sam.gielis@trustedfamily.net>
2021-12-12 12:16:20 +0100

    Auto-detect current category
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/components/ArticleCollection/CollectionHeader.tsx
+++ src/components/ArticleCollection/CollectionHeader.tsx
@@ -2,21 +2,23 @@ import React from 'react';
 import { HStack, Heading, Spacer, Box } from "@chakra-ui/layout"
 import { Select } from "@chakra-ui/react"
 import { SortMethod } from "."
+import { getCurrentCategory } from '../../utils/getCurrentCategory';
 
 interface CollectionHeaderProps {
     onSortMethodChanged(sortMethod: SortMethod): void;
 }
 
 export const CollectionHeader: React.FC<CollectionHeaderProps> = ({ children, onSortMethodChanged }) => {
+    const category = getCurrentCategory();
     return <HStack>
-        <Heading size="3xl" color={`hersenvulsel.${"default"}`} textTransform={'uppercase'}>{children}</Heading>
+        <Heading size="3xl" color={`hersenvulsel.${category}`} textTransform={'uppercase'}>{children}</Heading>
         <Spacer />
         <Box>
 
-        <Select size="lg" onChange={({ currentTarget }) => { onSortMethodChanged(currentTarget.value as SortMethod) }}>
-            <option defaultChecked value='NEWEST_FIRST'>Nieuwste eerst</option>
-            <option value='OLDEST_FIRST'>Oudste eerst</option>
-        </Select>
+            <Select size="lg" onChange={({ currentTarget }) => { onSortMethodChanged(currentTarget.value as SortMethod) }}>
+                <option defaultChecked value='NEWEST_FIRST'>Nieuwste eerst</option>
+                <option value='OLDEST_FIRST'>Oudste eerst</option>
+            </Select>
         </Box>
     </HStack>
 }
