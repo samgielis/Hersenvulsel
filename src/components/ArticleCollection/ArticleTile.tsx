71c06ca5dc56e6bde6fc80a54f21cbc608106fdb
Sam Gielis <sam.gielis@trustedfamily.net>
2021-12-12 16:45:24 +0100

    Use LinkBox for ArticleHighlight and ArticleTile
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/components/ArticleCollection/ArticleTile.tsx
+++ src/components/ArticleCollection/ArticleTile.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AspectRatio, Box, Heading, Text } from "@chakra-ui/layout";
+import { AspectRatio, Box, Heading, LinkBox, LinkOverlay, Text } from "@chakra-ui/layout";
 import Img, { FluidObject } from "gatsby-image";
 import { Category } from '../../types/Category';
 import { Link } from '../Link';
@@ -15,36 +15,41 @@ export interface ArticleTileData {
 
 export const ArticleTile = ({ title, category, id, image }: Omit<ArticleTileData, "publishDate">) => {
     return (
-        <AspectRatio ratio={16 / 9} backgroundColor="black">
-            <Box>
-                {image ? (
-                    <Img
-                        alt={title}
-                        fluid={image}
-                        style={{
-                            width: '100%',
-                            height: "100%",
-                            opacity: ".7"
-                        }}
-                    />
-                ) : (
-                    <div />
-                )}
-                <b>
-                    <Text textTransform="uppercase" className="hv-tile-category">{category}</Text>
-                </b>
+        <LinkBox>
+            <AspectRatio ratio={16 / 9} backgroundColor="black">
+                <Box>
+                    {image ? (
+                        <Img
+                            alt={title}
+                            fluid={image}
+                            style={{
+                                width: '100%',
+                                height: "100%",
+                                opacity: ".7"
+                            }}
+                        />
+                    ) : (
+                        <div />
+                    )}
+                    <b>
+                        <Text textTransform="uppercase" className="hv-tile-category">{category}</Text>
+                    </b>
 
-                <Heading m={2} size="2xl" textAlign="center" as="h2" fontWeight="normal" className="hv-tile-title hv-tile-title-default">
-                    <span>
-                        <Link
-                            to={`/${category}/${id}/`}
-                            className="thumblink"
-                        >
-                            {title}
-                        </Link>
-                    </span>
-                </Heading>
-            </Box>
-        </AspectRatio>
+                    <Heading m={2} size="2xl" textAlign="center" as="h2" fontWeight="normal" className="hv-tile-title hv-tile-title-default">
+                        <LinkOverlay as={Link} to={`/${category}/${id}/`} _hover={{textDecor: "none"}}>
+                            <span>
+
+                                <Link
+                                    to={`/${category}/${id}/`}
+                                    className="thumblink"
+                                >
+                                    {title}
+                                </Link>
+                            </span>
+                        </LinkOverlay>
+                    </Heading>
+                </Box>
+            </AspectRatio>
+        </LinkBox>
     );
 };
--- src/components/ArticleHighlight/index.tsx
+++ src/components/ArticleHighlight/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { AspectRatio, Box, Heading, Link, SimpleGrid, Spacer, Stack, Text } from "@chakra-ui/layout"
+import { AspectRatio, Box, Heading, LinkBox, LinkOverlay, SimpleGrid, Spacer, Stack, Text } from "@chakra-ui/layout"
 import { ArticleTileData } from "../ArticleCollection/ArticleTile"
 import Img from "gatsby-image";
 import CategoryTitle from "../CategoryTitle";
-import { Link as GatsbyLink } from 'gatsby';
+import { Link } from '../Link';
 
 interface ArticleHighlightProps {
     article: ArticleTileData;
@@ -11,7 +11,9 @@ interface ArticleHighlightProps {
 
 export const ArticleHighlight = ({ article }: ArticleHighlightProps) => {
     const { image, title, category, id, publishDate, excerpt } = article;
-    return <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+    const articleURL = `/${category}/${id}/`;
+    return <LinkBox>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
         <AspectRatio ratio={16 / 11}>
             {image && <Img
                 alt={title}
@@ -25,17 +27,20 @@ export const ArticleHighlight = ({ article }: ArticleHighlightProps) => {
             <Stack minH="100%" spacing={10}>
                 <Box>
                     <CategoryTitle category={category} />
-                    <Heading size="3xl">{title}</Heading>
+                    <LinkOverlay as={Link} to={articleURL} _hover={{ color: `black` }}>
+                        <Heading size="3xl">{title}</Heading>
+                    </LinkOverlay>
                 </Box>
                 <Spacer />
                 <Box>
                     <Text fontSize="3xl" fontWeight="bold">{publishDate.toLocaleDateString()}</Text>
                     <Text fontSize="4xl" >{excerpt}</Text>
-                    <Link fontSize="2xl" as={GatsbyLink} to={`/${category}/${id}/`} _hover={{ color: `hersenvulsel.${category}` }} color={`hersenvulsel.${category}`} textTransform="uppercase" fontWeight="bold">
+                    <Link fontSize="2xl" to={articleURL} _hover={{ color: `hersenvulsel.${category}` }} color={`hersenvulsel.${category}`} textTransform="uppercase" fontWeight="bold">
                         Lees meer
                     </Link>
                 </Box>
             </Stack>
         </Box>
     </SimpleGrid>
+    </LinkBox>
 }
