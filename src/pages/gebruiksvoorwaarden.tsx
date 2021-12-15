3a5440e0e0d8d387affb48500660e67ea3a10db7
Sam Gielis <sam.gielis@trustedfamily.net>
2021-12-15 15:13:51 +0100

    Fix info pages sizes
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/pages/gebruiksvoorwaarden.tsx
+++ src/pages/gebruiksvoorwaarden.tsx
@@ -6,112 +6,112 @@ import Layout from '../layouts/default-layout';
 const NotFoundPage = (): JSX.Element => (
     <Layout>
         <SEO title="Gebruiksvoorwaarden" />
-        <Stack spacing={10}>
-            <Heading as="h1" size='3xl' textTransform="uppercase">Gebruiksvoorwaarden</Heading>
+        <Stack spacing={5}>
+            <Heading as="h1" size='xl' textTransform="uppercase">Gebruiksvoorwaarden</Heading>
 
-            <Heading size="2xl">Overeenkomst tussen de gebruiker en Hersenvulsel</Heading>
-            <Text fontSize="3xl">
+            <Heading size="xl">Overeenkomst tussen de gebruiker en Hersenvulsel</Heading>
+            <Text fontSize="xl">
                 De Hersenvulsel website bestaat uit verschillende webpagina’s die worden beheerd door Hersenvulsel. De website wordt u aangeboden onder de voorwaarde dat u deze accepteert zonder de hierin vermelde voorwaarden, bepalingen en mededelingen. Uw gebruik van de Hersenvulsel website vormt uw akkoord met al deze voorwaarden, bepalingen en mededelingen te wijzigen.
             </Text>
 
-            <Heading size="2xl">Verandering van deze algemene voorwaarden</Heading>
-            <Text fontSize="3xl">
+            <Heading size="xl">Verandering van deze algemene voorwaarden</Heading>
+            <Text fontSize="xl">
                 Hersenvulsel behoudt zich het recht om de voorwaarden, bepalingen en mededelingen volgens welke de Hersenvulsel website wordt aangeboden te veranderen, met inbegrip van maar niet beperkt tot kosten die worden geassocieerd met het gebruik van de Hersenvulsel website.
             </Text>
 
-            <Heading size="2xl">Links naar websites beheerd door derden</Heading>
-            <Text fontSize="3xl">
+            <Heading size="xl">Links naar websites beheerd door derden</Heading>
+            <Text fontSize="xl">
                 De Hersenvulsel website kan links naar andere websites (“Gelinkte Sites”) bevatten. De gelinkte sites zijn niet onder de controle van Hersenvulsel en Hersenvulsel is niet verantwoordelijk voor de inhoud van enige gelinkte site, met inbegrip van maar niet beperkt tot eventuele links die aanwezig zijn op een gelinkte site, dan wel wijzigingen of updates van een gelinkte site. Hersenvulsel is niet verantwoordelijk voor webcasting of andere vormen van overdracht vanaf een gelinkte site. Hersenvulsel verstrekt deze links uitsluitend voor uw gemak, en het opnemen van een link impliceert op geen enkele wijze de goedkeuring van Hersenvulsel van de betreffende site noch enige connectie met de exploitanten ervan.
             </Text>
 
-            <Heading size="2xl">Geen onrechtmatig of verboden gebruik</Heading>
-            <Text fontSize="3xl">
+            <Heading size="xl">Geen onrechtmatig of verboden gebruik</Heading>
+            <Text fontSize="xl">
                 Als voorwaarde aan het gebruik van de Hersenvulsel website, garandeert u Hersenvulsel dat u de Hersenvulsel website niet zult gebruiken voor enig onrechtmatig doel of enig doel dat ingevolge deze voorwaarden, bepalingen en mededelingen verboden is. U mag de Hersenvulsel website niet gebruiken op een manier waardoor schade, storing, overbelasting of aantasting veroorzaakt wordt aan de Hersenvulsel website of het gebruik en genot van de Hersenvulsel website van een andere partij wordt verstoord. U mag op geen enkele wijze via de Hersenvulsel websites materialen of informatie verkrijgen of trachten te verkrijgen indien de betreffende materialen of informatie niet voor dat doel zijn verstrekt of beschikbaar zijn gesteld.
             </Text>
 
-            <Heading size="2xl">Gebruik communicatiediensten</Heading>
-            <Text fontSize="3xl">
+            <Heading size="xl">Gebruik communicatiediensten</Heading>
+            <Text fontSize="xl">
                 De Hersenvulsel website kan bulletin board services, chat areas, nieuwsgroepen, forums, communities, persoonlijke webpagina’s, kalenders, en/of andere berichten- of communicatiefaciliteiten bevatten ontworpen om u in staat te stellen te communiceren met het grote publiek of met een groep. (collectief, “Communicatiediensten”), u gaat akkoord met het gebruik van de communicatiediensten om alleen maar berichten en materialen die gepast zijn en die verbonden zijn met de betreffende communicatiedienst. Als voorbeeld en niet als beperking, gaat u akkoord, dat wanneer u een communicatiedienst gebruikt, u niet zult:
             </Text>
 
-            <Text fontSize="3xl">
+            <Text fontSize="xl">
                 te schande maken, misbruiken, treiteren, stalken, bedreigen noch op een andere manier de rechten van anderen (zoals rechten op privacy en publiciteit) schenden. Publiceren, plaatsen, uploaden, distribueren of verspreiden enig ongepast, profaan, lasterlijk, schendend, obsceen of aanstootgevend of illegaal onderwerp, naam, materiaal of informatie. Bestanden uploaden naar de website die software of andere materialen bevatten die beschermd zijn door intellectuele eigendomsrechten (of door de rechten op privacy en publiciteit), tenzij u houder bent van de voornoemde rechten of er een wettelijke controle over uitoefent, of u alle noodzakelijke toelatingen ontvangen hebt.
             </Text>
 
-            <Text fontSize="3xl">
+            <Text fontSize="xl">
                 Bestanden uploaden die virussen, corrupte bestanden, of enige andere gelijksoortige software of programma’s, die de werking van een ander’s computer kunnen schaden, bevatten. Goederen of diensten adverteren of te koop aanbieden of kopen van voor enig bedrijfsdoel, tenzij de Communicatiedienst zulke berichten toestaat.
             </Text>
 
-            <Text fontSize="3xl">
+            <Text fontSize="xl">
                 Enquêtes, wedstrijden, piramidesystemen of kettingbrieven uitvoeren of doorsturen. Enig bestand downloaden geplaatst door een andere gebruiker van een Communicatiedienst, waarvan u weet of het aannemelijk is dat u behoort te weten, dat deze niet op deze wijze legaal gedistribueerd kan worden.
             </Text>
 
-            <Text fontSize="3xl">
+            <Text fontSize="xl">
                 Toewijzingen van auteurs, juridische of andere gepaste mededelingen of aanduidingen van eigendom of het etiket van de herkomst of bron van de software of ander materiaal dat het bestand, gekregen middels uploaden, bevat, falsificeren of wissen.
             </Text>
 
-            <Text fontSize="3xl">
+            <Text fontSize="xl">
                 Een andere gebruiker in het gebruiken of genieten van de Communicatiediensten beperken of remmen. Een gedragscode of andere richtlijnen die van toepassing kunnen zijn op een betreffende Communicatiedienst schenden.
             </Text>
 
-            <Text fontSize="3xl">
+            <Text fontSize="xl">
                 Oogsten of op een andere manier informatie verzamelen over anderen, met inbegrip van e-mail adressen, zonder hun toestemming. Handelen in strijd met toepasselijk recht of wettelijke bepalingen.
             </Text>
 
-            <Text fontSize="3xl">
+            <Text fontSize="xl">
                 Hersenvulsel is niet verplicht toezicht te houden op de Communicatiediensten. Hersenvulsel behoudt echter het recht om materialen geplaatst op een Communicatiedienst te beoordelen en materialen naar eigen goeddunken te verwijderen. Hersenvulsel behoudt het recht om uw toegang tot enige of alle Communicatiediensten te beëindigen op welk tijdstip dan ook, zonder mededeling, voor welke reden dan ook.
             </Text>
 
-            <Text fontSize="3xl">
+            <Text fontSize="xl">
                 Hersenvulsel behoudt het recht om naar Hersenvulsel’s eigen goeddunken op elk moment informatie vrij te geven als dit nodig is om te voldoen aan toepasselijk recht, een wettelijke bepaling, juridische procedure of overheidsaanvraag, of om informatie of materialen, geheel of gedeeltelijk te redigeren, weigeren te plaatsen of te verwijderen.
             </Text>
 
-            <Text fontSize="3xl">
+            <Text fontSize="xl">
                 Voorzichtigheid is geboden wanneer u persoonlijk identificerende informatie over uzelf of uw kinderen geeft in een Servicedienst. Hersenvulsel heeft geen controle over de inhoud, berichten of informatie in een Communicatiedienst of keurt deze goed en daarom kan Hersenvulsel niet verantwoordelijk worden gehouden wat betreft de Communicatiediensten en voor handelingen die volgen uit uw deelname aan een Communicatiedienst. Managers en hosts zijn geen gemachtigde Hersenvulsel woordvoerders, en hun standpunten komen niet noodzakelijk overeen met die van Hersenvulsel.
             </Text>
 
-            <Text fontSize="3xl">
+            <Text fontSize="xl">
                 Materialen geupload naar een Communicatiedienst kunnen ondergeschikt zijn aan geplaatste gebruikers-, reproductie- en/of verspreidingsbeperkingen. U bent verantwoordelijk voor de naleving van dergelijke beperkingen als u de materialen downloadt.
             </Text>
 
-            <Heading size="2xl">Materialen verstrekt aan Hersenvulsel of geplaatst op een Hersenvulsel website</Heading>
-            <Text fontSize="3xl">
+            <Heading size="xl">Materialen verstrekt aan Hersenvulsel of geplaatst op een Hersenvulsel website</Heading>
+            <Text fontSize="xl">
                 Hersenvulsel beschouwt de materialen die u verstrekt aan Hersenvulsel (inclusief feedback en suggesties) of plaatst, uploadt, toevoegt of overlegt aan een Hersenvulsel Website of daarmee samenhangende diensten (collectief: “Inzending”), niet als eigendom. Echter, met het plaatsen, uploaden, toevoegen of het aanbieden van de Inzendingen, geeft u Hersenvulsel, en de daaraan verbonden bedrijven en de nodige onderlicenties, toestemming om uw Inzending te gebruiken voor hun Internetbedrijf met inbegrip van, zonder beperking, het recht om te: kopiëren, distribueren, overbrengen, publiekelijk tonen, publiekelijk uitvoeren, reproduceren, redigeren, vertalen en opnieuw formatteren van uw Inzending; en het bekend maken van uw naam met betrekking tot uw Inzending.
             </Text>
 
-            <Text fontSize="3xl">
+            <Text fontSize="xl">
                 Er zal geen vergoeding worden betaald met betrekking tot het gebruik van uw Inzending, zoals hierin voorzien is. Hersenvulsel is niet verplicht uw Inzending te plaatsen of uw Inzending te gebruiken en mag de Inzending op elk moment naar Hersenvulsel’s eigen goeddunken verwijderen.
             </Text>
 
-            <Text fontSize="3xl">
+            <Text fontSize="xl">
                 Met het plaatsen, uploaden, toevoegen, verstrekken van of het inzenden van uw Inzending, garandeert u en bevestigt u dat u de houder bent van de rechten over uw Inzending of dat u er een wettelijke controle over uitoefent zoals beschreven in deze sectie, met inbegrip van, zonder beperking, alle rechten noodzakelijk voor u om uw Inzending te verstrekken, plaatsen, uploaden, toe te voegen of in te zenden.
             </Text>
 
-            <Heading size="2xl">Uitsluiting van aansprakelijkheid</Heading>
-            <Text fontSize="3xl">
+            <Heading size="xl">Uitsluiting van aansprakelijkheid</Heading>
+            <Text fontSize="xl">
                 DE INFORMATIE, SOFTWARE, PRODUCTEN EN DIENSTEN OP OF BESCHIKBAAR GEMAAKT VIA DE Hersenvulsel WEBSITE KUNNEN ONNAUWKEURIGHEDEN OF TYPOGRAFISCHE FOUTEN BEVATTEN. VERANDERINGEN WORDEN PERIODIEK TOEGEVOEGD AAN DE INFORMATIE. Hersenvulsel EN/OF HAAR LEVERANCIERS KUNNEN TE ALLEN TIJDE VERBETERINGEN EN/OF WIJZIGINGEN AANBRENGEN IN DE Hersenvulsel WEBSITE. ER DIENT NIET TE WORDEN VERTROUWD OP ADVIEZEN DIE WORDEN ONTVANGEN VIA DE Hersenvulsel WEBSITE VOOR HET NEMEN VAN BESLISSINGEN VAN PERSOONLIJKE, MEDISCHE, JURIDISCHE OF FINANCIELE AARD EN U DIENT EEN TER ZAKE DESKUNDIGE TE RAADPLEGEN VOOR EEN OP UW SITUATIE TOEGESPITST ADVIES.
             </Text>
 
-            <Text fontSize="3xl">
+            <Text fontSize="xl">
                 Hersenvulsel EN HAAR LEVERANCIERS STAAN NIET IN VOOR DE GESCHIKTHEID, BETROUWBAARHEID, TIJDIGHEID, EN DE NAUWKEURIGEHID VAN DE INFORMATIE, SOFTWARE, PRODUCTEN, DIENSTEN EN DAARMEE VERBAND HOUDENDE GRAFISCHE GEGEVENS OPGENOMEN OP DE Hersenvulsel WEBSITE, VOOR WELKE TOEPASSING DAN OOK. VOOR ZOVER MAXIMAAL TOEGESTAAN OP GROND VAN TOEPASSELIJK RECHT, WORDEN AL DERGELIJKE INFORMATIE, SOFTWARE, PRODUCTEN, DIENSTEN EN DAARMEE SAMENHANGENDE GRAFISCHE GEGEVENS GELEVERD ‘IN DE HUIDIGE STAAT’ EN ZONDER ENIGE VORM VAN GARANTIE. Hersenvulsel EN/OF HAAR LEVERANCIERS WIJZEN HIERBIJ ALLE GARANTIES VAN DE HAND MET BETREKKING TOT DEZE INFORMATIE, SOFTWARE, PRODUCTEN, DIENSTEN EN DAARMEE SAMENHANGENDE GRAFISCHE GEGEVENS, MET INBEGRIP VAN ALLE IMPLICIETE GARANTIES BETREFFENDE DE VERKOOPBAARHEID, GESCHIKTHEID VOOR EEN BEPAALD DOEL, EIGENDOM EN NIET-INBREUKMAKENDHEID.
             </Text>
 
-            <Text fontSize="3xl">
+            <Text fontSize="xl">
                 VOOR ZOVER MAXIMAAL TOEGESTAAN OP GROND VAN TOEPASSELIJK RECHT, ZIJN Hersenvulsel EN/OF HAAR LEVERANCIERS IN GEEN GEVAL AANSPRAKELIJK VOOR ENIGE DIRECTE, INDIRECTE, PUNITIEVE, INCIDENTELE, BIJZONDERE SCHADE, GEVOLGSCHADE OF ENIGE ANDERE SCHADE MET INBEGRIP VAN, MAAR NIET BEPERKT TOT SCHADE TEN GEVOLGE VAN HET VERLIES VAN GEBRUIK OF GEGEVENS VOORTVLOEIEND UIT OF OP EEN ANDERE WIJZE VERBAND HOUDEN MET, HET GEBRUIK VAN DE Hersenvulsel WEBSITE, MET DE VERTRAGING OF HET NIET KUNNEN GEBRUIKEN VAN DE Hersenvulsel WEBSITE OF DAARMEE SAMENHANGENDE DIENSTEN, HET VERLENEN OF HET NIET KUNNEN VERLENEN VAN DIENSTEN, NOCH VOOR ENIGE INFORMATIE, SOFTWARE, PRODUCTEN, DIENSTEN EN DAARMEE SAMENHANGENDE GRAFISCHE GEGEVENS DIE VIA DE Hersenvulsel WEBSITE WORDEN VERKREGEN OF DIE ANDERSZINS VOORTVLOEIEN UIT HET GEBRUIK VAN DE Hersenvulsel WEBSITE, HETZIJ OP BASIS VAN CONTRACT, ONRECHTMATIGE DAAD, NALATIGHEID, RISICOAANSPRAKELIJKHEID OF ANDERSZINS, ZELFS ALS Hersenvulsel WEBSITE VAN DE MOGELIJKHEID VAN SCHADE OP DE HOOGTE IS GESTELD. AANGEZIEN SOMMIGE RECHTSSYSTEMEN UITSLUITING OF BEPERKING VAN AANSPRAKELIJKHEID VOOR GEVOLGSCHADE OF INCIDENTELE SCHADE NIET TOESTAAN, IS HET MOGELIJK DAT DE VOORGAANDE BEPERKING OP U NIET VAN TOEPASSING IS. INDIEN U NIET TEVREDEN BENT OVER ENIGE GEDEELTE VAN DE Hersenvulsel WEBSITE, OF MET EEN VAN DEZE GEBRUIKSVOORWAARDEN, IS UW ENIGE EN UITSLUITENDE VERHAALSMOGELIJKHEID HET GEBRUIK VAN DE Hersenvulsel WEBSITE TE STAKEN. CONTACT: info@hersenvulsel.be
             </Text>
 
-            <Heading size="2xl">Kennisgeving inzake auteurs- en merkenrecht</Heading>
-            <Text fontSize="3xl">
+            <Heading size="xl">Kennisgeving inzake auteurs- en merkenrecht</Heading>
+            <Text fontSize="xl">
                 De inhoud van de Hersenvulsel website is: Van Copyright 2016-heden Hersenvulsel.be en/of haar leveranciers. Alle rechten voorbehouden.
             </Text>
 
-            <Heading size="2xl">Handelsmerken</Heading>
-            <Text fontSize="3xl">
+            <Heading size="xl">Handelsmerken</Heading>
+            <Text fontSize="xl">
                 De namen van de hierin genoemde werkelijke bedrijven en producten kunnen de handelsmerken van hun respectievelijke eigenaars zijn. De hierin als voorbeeld genoemde bedrijven, organisaties, producten, mensen en gebeurtenissen zijn fictief. Het is niet de bedoeling, noch moet dit worden afgeleid, een associatie te maken met een echt bedrijf, organisatie, product, persoon of gebeurtenis. Rechten niet uitdrukkelijk gegeven hier, zijn voorbehouden.
             </Text>
 
-            <Heading size="2xl">Mededeling en procedure voor het instellen van vorderingen inzake inbreuk op het auteursrecht.</Heading>
-            <Text fontSize="3xl">
+            <Heading size="xl">Mededeling en procedure voor het instellen van vorderingen inzake inbreuk op het auteursrecht.</Heading>
+            <Text fontSize="xl">
                 Krachtens Titel 17 van het Wetboek van de Verenigde Staten, Artikel 512(c)(2) dienen kennisgevingen betreffende een vermeende schending van het auteursrecht te worden gestuurd naar de daarvoor aangewezen vertegenwoordiger van de Serviceprovider. ER WORDT GEEN ANTWOORD GEGEVEN OP VRAGEN DIE GEEN VERBAND HOUDEN MET DE VOLGENDE PROCEDURE. Zie Mededelingen en procedure voor het instellen van vorderingen inzake inbreuk op het auteursrecht.
             </Text>
         </Stack>
